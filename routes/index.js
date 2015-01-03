98816fe4cb620969f3ad10f459ddff3fbf2e6812
Joseph Post <jp@jsphpst.com>
2015-01-03 01:16:23 -0600

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'jcpst.com' });
+});
+
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'jcpst.com' });
+});
+
+router.get('/music', function(req, res) {
+  res.render('music', { title: 'jcpst.com' });
+});
+
+router.get('/contact', function(req, res) {
+  res.render('contact', { title: 'jcpst.com' });
+});
+
+module.exports = router;
