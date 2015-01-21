74eec590b4563faf6661196a7a64ad1d3da965d4
Joseph Post <jp@jsphpst.com>
2015-01-21 00:54:46 -0500

    fix merge issue

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,23 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-<<<<<<< HEAD
-  res.render('index', { title: 'jcpst.com' });
+router.get('/', function (req, res) {
+    res.render('index', { title: 'jcpst.com' });
 });
 
-router.get('/about', function(req, res) {
-  res.render('about', { title: 'jcpst.com' });
+router.get('/about', function (req, res) {
+    res.render('about', { title: 'jcpst.com' });
 });
 
-router.get('/music', function(req, res) {
-  res.render('music', { title: 'jcpst.com' });
+router.get('/music', function (req, res) {
+    res.render('music', { title: 'jcpst.com' });
 });
 
-router.get('/contact', function(req, res) {
-  res.render('contact', { title: 'jcpst.com' });
-=======
-  res.render('index', { title: 'Express' });
->>>>>>> c2c3846489e15ca5b13cd1b6497383c8635b1f77
+router.get('/contact', function (req, res) {
+    res.render('contact', { title: 'jcpst.com' });
 });
 
 module.exports = router;
